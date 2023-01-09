e255792ad1e5330f1460a7f837156691b22b5429
Kimdonggyu <qweszx33@naver.com>
2023-01-09 11:51:56 +0900

    error 수정

--- src/Components/Nav_Hwang/test.js
+++ src/Components/Nav_Hwang/test.js
@@ -2,25 +2,7 @@
 function test() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi. We are Dot Note
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-  
-          <h1 class="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-          <Button type="primary">Button</Button>
-        </header>
+        test 수정
       </div>
     );
   }
