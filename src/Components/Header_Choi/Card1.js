a19969ef0481f98960931a416f9e3d09eb31e3ee
soy3000 <990216c@gmail.com>
2023-01-11 13:09:34 +0900

    finish

---
+++ src/Components/Header_Choi/Card1.js
@@ -0,0 +1,17 @@
+
+function Card1(){
+    return(
+
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="Assets/IMG/card.jpg" alt="img sample"></img>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">時代はメタバースの時代！</div>
+    <p className="text-gray-700 text-base">
+    メタバースへようこそ！
+    </p>
+  </div>
+</div>
+)
+}
+
+export default Card1;
--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,9 @@
-
+import Card1 from "./Components/Header_Choi/Card1";
 
 function Test() {
   return (
     <div className="h-screen w-screen">
+     <Card1></Card1>
     </div>
   );
 }
